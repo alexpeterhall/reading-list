074e5ef4eacf298cfe9cae537c3dbdc1fd73bb77
Alex Hall <alexpeterhall@gmail.com>
2022-11-04 22:44:14 -0400

    Remove negative tests covered by type safety

--- tests/totalPages.test.js
+++ tests/totalPages.test.js
@@ -1,24 +1,10 @@
-'use strict';
-const BookStats = require('../scripts/BookStats');
+'use strict'
+const BookStats = require('../scripts/BookStats.js')
 
-const sunnyDay = [
-      { pages: 100 },
-      { pages: 200 },
-      { pages: 300 },
-      { pages: 400 },
-    ],
-    pagesAsStrings = [
-      { pages: "100" },
-      { pages: "200" },
-      { pages: "300" },
-      { pages: "400" },
-    ];
-    
+const sunnyDay = [{ pages: 100 }, { pages: 200 }, { pages: 300 }, { pages: 400 }]
+
 describe('Total Pages Suite', () => {
   test('Sums page numbers from 4 books to equal 1000', () => {
-    expect(BookStats.getTotalPagesRead(sunnyDay)).toBe(1000);
-  });
-  test('Sums pages as strings from 4 books to equal 1000', () => {
-    expect(BookStats.getTotalPagesRead(pagesAsStrings)).toBe(1000);
-  });
-});
+    expect(BookStats.getTotalPagesRead(sunnyDay)).toBe(1000)
+  })
+})
--- tests/yearsRead.test.js
+++ tests/yearsRead.test.js
@@ -1,61 +1,27 @@
-'use strict';
-const BookStats = require('../scripts/BookStats');
+'use strict'
+const BookStats = require('../scripts/BookStats.js')
 
 const yearsArray = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
-      sunnyDay = [
-        { read: { year: 1911 } },
-        { read: { year: 1986 } },
-        { read: { year: 2005 } },
-        { read: { year: 2009 } },
-      ],
-      yearsAsStrings = [
-        { read: { year: "1911" } },
-        { read: { year: "1986" } },
-        { read: { year: "2005" } },
-        { read: { year: "2009" } },
-      ],
-      badInputs = [
-        { read: { year: null } },
-        { read: { year: undefined } },
-        { read: { year: true } },
-        { read: { year: false } },
-        { read: { year: [ "2009" ] } },
-        { read: { year: { year: "2009" } } },
-        { read: { year: "2009" } },
-        { read: { year: 2011 } },
-        { read: { year: "2014" } },
-        { read: { year: 1911 } },
-      ];
+  sunnyDay = [
+    { dates_read: [{ month: 1, year: 1911 }] },
+    { dates_read: [{ month: undefined, year: 1918 }] },
+    { dates_read: [{ month: 12, year: 1986 }] },
+    { dates_read: [{ month: 3, year: 2005 }] },
+    { dates_read: [{ month: 9, year: 2009 }] },
+    { dates_read: [{ month: null, year: 2009 }] },
+    { dates_read: [{ month: 1, year: 2011 }] },
+    { dates_read: [{ month: 1, year: null }] },
+    { dates_read: [{ month: 1, year: undefined }] },
+  ]
 
-describe('First Year Read Suite', () => {
+describe('Years Read Suite', () => {
   test('Finds earliest year in nested object to equal 1911', () => {
-    expect(BookStats.getFirstYearRead(sunnyDay)).toBe(1911);
-  });
-  test('Finds earliest year in nested object and returns a number', () => {
-    expect(BookStats.getFirstYearRead(yearsAsStrings)).toBe(1911);
-  });
-  test('Finds earliest year and returns a number when given improper inputs', () => {
-    expect(BookStats.getFirstYearRead(badInputs)).toBe(1911);
-  });  
-});
-
-describe('Last Year Read Suite', () => {
+    expect(BookStats.getFirstYearRead(sunnyDay)).toBe(1911)
+  })
   test('Finds latest year in nested object to equal 2009', () => {
-    expect(BookStats.getLastYearRead(sunnyDay)).toBe(2009);
-  });
-  test('Finds latest year in nested object and returns a number', () => {
-    expect(BookStats.getLastYearRead(yearsAsStrings)).toBe(2009);
-  });
-  test('Finds latest year and returns a number when given improper inputs', () => {
-    expect(BookStats.getLastYearRead(badInputs)).toBe(2014);
-  });
-});
-
-describe('All Years Read Suite', () => {
+    expect(BookStats.getLastYearRead(sunnyDay)).toBe(2011)
+  })
   test('Returned array contains all expected values between provided numbers', () => {
-    expect(BookStats.getYearsReading(2010, 2020)).toEqual(yearsArray);
-  });
-  test('Returned array contains all expected values between provided strings', () => {
-    expect(BookStats.getYearsReading('2010', '2020')).toEqual(yearsArray);
-  });
-});
+    expect(BookStats.getYearsReading(2010, 2020)).toEqual(yearsArray)
+  })
+})
