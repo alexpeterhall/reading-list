d42d49fc35051e3beb53f62bc18d5ec73c1c8fb9
Alex Hall <alexpeterhall@gmail.com>
2022-12-14 10:37:51 -0500

    Add tests for counting pages of books read multiple times

--- tests/totalPages.test.js
+++ tests/totalPages.test.js
@@ -1,10 +1,64 @@
 'use strict'
 const BookStats = require('../scripts/BookStats.js')
 
-const sunnyDay = [{ pages: 100 }, { pages: 200 }, { pages: 300 }, { pages: 400 }]
+const sunnyDay = [
+  { pages: 100, dates_read: [{ month: 1, year: 2010 }] },
+  { pages: 200, dates_read: [{ month: 1, year: 2010 }] },
+  { pages: 300, dates_read: [{ month: 1, year: 2010 }] },
+  { pages: 400, dates_read: [{ month: 1, year: 2010 }] },
+]
+const multipleReads = [
+  {
+    pages: 100,
+    dates_read: [
+      { month: 1, year: 2010 },
+      { month: 12, year: 1986 },
+    ],
+  },
+  {
+    pages: 200,
+    dates_read: [
+      { month: 12, year: 1786 },
+      { month: 1, year: 1515 },
+    ],
+  },
+  {
+    pages: 300,
+    dates_read: [
+      { month: 3, year: 1882 },
+      { month: 4, year: 1500 },
+      { month: 12, year: 1670 },
+    ],
+  },
+]
+const badInputs = [
+  {
+    pages: 100,
+    dates_read: [
+      { month: 1, year: 2022 },
+      { month: 14, year: 3 },
+      { month: null, year: null },
+      { month: undefined, year: undefined },
+    ],
+  },
+  {
+    pages: 200,
+    dates_read: null,
+  },
+  {
+    pages: 300,
+    dates_read: undefined,
+  },
+]
 
-describe('Total Pages Suite', () => {
-  test('Sums page numbers from 4 books to equal 1000', () => {
+describe('Total Pages', () => {
+  test('Sums pages for books read only once', () => {
     expect(BookStats.getTotalPagesRead(sunnyDay)).toBe(1000)
+  })
+  test('Sum pages for books read multiple times', () => {
+    expect(BookStats.getTotalPagesRead(multipleReads)).toBe(1500)
+  })
+  test('Sums pages when books have unexpected data inputs', () => {
+    expect(BookStats.getTotalPagesRead(badInputs)).toBe(200)
   })
 })
