379c5d5f4eb2af289bd1843657213859b50919d6
Alex Hall <alexpeterhall@gmail.com>
2022-12-14 14:07:49 -0500

    Remove unnecessary variables

--- printStats.ts
+++ printStats.ts
@@ -3,10 +3,7 @@ import READING_LIST from './ReadingList.json'
 const BOOKS: book[] = READING_LIST,
   numberOfBooksReadByYear = BookStats.getNumberOfBooksReadByYear(BOOKS),
   totalPagesRead = BookStats.getTotalPagesRead(BOOKS),
-  totalYearsReading = Object.keys(numberOfBooksReadByYear).length,
-  averageYear = totalPagesRead / totalYearsReading,
-  averageWeek = totalPagesRead / (totalYearsReading * 52),
-  averageDay = totalPagesRead / (totalYearsReading * 365)
+  totalYearsReading = Object.keys(numberOfBooksReadByYear).length
   
 //TODO Save stats for each year into a typed object
 
@@ -14,24 +11,21 @@ function printStats(year?: number) {
   console.log('Total Books Read: ' + BOOKS.length)
   console.log('Total Years Reading: ' + totalYearsReading)
   console.log('Total Pages Read: ' + totalPagesRead)
-  console.log('Average Pages Per Year: ' + averageYear.toFixed(2))
-  console.log('Average Pages Per Week: ' + averageWeek.toFixed(2))
-  console.log('Average Pages Per Day: ' + averageDay.toFixed(2))
+  console.log('Average Pages Per Year: ' + (totalPagesRead / totalYearsReading).toFixed(2))
+  console.log('Average Pages Per Week: ' + (totalPagesRead / (totalYearsReading * 52)).toFixed(2))
+  console.log('Average Pages Per Day: ' + (totalPagesRead / (totalYearsReading * 365)).toFixed(2))
   console.log('Number of Books Read by Year:')
   // Prints the object because it looks better than a concatenated string in console.
   console.log(numberOfBooksReadByYear)
   // If optional year is provided and there is data for that year, print the yearly stats.
   if (year && numberOfBooksReadByYear[year]) {
-      const booksReadInYear = BookStats.getBooksReadByYear(BOOKS, year)
       const totalPagesForYear = BookStats.getPagesReadByYear(BOOKS, year)
-      const averageWeekForYear = totalPagesForYear / 52
-      const averageDayForYear = totalPagesForYear / 365
       console.log('Total Pages Read for Year ' + year + ': ' + totalPagesForYear)
-      console.log('Average Pages Per Week for Year ' + year + ': ' + averageWeekForYear.toFixed(2))
-      console.log('Average Pages Per Day for Year ' + year + ': ' + averageDayForYear.toFixed(2))
+      console.log('Average Pages Per Week for Year ' + year + ': ' + (totalPagesForYear / 52).toFixed(2))
+      console.log('Average Pages Per Day for Year ' + year + ': ' + (totalPagesForYear / 365).toFixed(2))
       console.log('Books Read in Year ' + year + ': ')
       // Prints the array because it looks better than a concatenated string in console.
-      console.log(booksReadInYear)
+      console.log(BookStats.getBooksReadByYear(BOOKS, year))
     } else {
       console.warn('Year provided does not have any data.')
     }
